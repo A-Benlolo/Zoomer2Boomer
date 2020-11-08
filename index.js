bfbed93af0578cfe91e348a4e8b0f5c2a6a83063
A-Benlolo <alexbenlolo@gmail.com>
2020-11-07 23:54:25 -0500

    Added UrbanDictionary scraping

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const mysql = require('mysql');
 const express = require('express')
+const puppeteer = require('puppeteer');
 
+// Create a MySQL connection
 const connection = mysql.createConnection({
     host: "localhost",
     user: "root",
@@ -8,20 +10,124 @@ const connection = mysql.createConnection({
     database: "z2b"
 });
 
+// Connect the MySQL database to the server
 connection.connect(err => {
     if(err) throw err
     console.log("MySQL Connected");
 });
 
+// Initialize express
 const app = express();
 
+// Setup express
 app.listen('3000', () => {console.log("Server Started on port 3000")});
 app.use(express.static('public'));
+app.set('view engine', 'pug')
 
+// Create the GET to query for a term and return the corresponding rows
 app.get('/query/:term', (request, response) => {
-    let sql = `SELECT * FROM word WHERE term='${request.params.term}'`
+    let sql = `SELECT * FROM word WHERE term="${request.params.term}"`
     connection.query(sql, (err, result) => {
         if(err) throw err
         response.json(result)
+    })
+})
+
+// Create the GET to scrape for the definition of a term
+app.get('/scrape/:term', (request, response) => {
+    puppeteer.launch().then(async function(browser) {
+        const page = await browser.newPage();
+        await page.goto("https://api.urbandictionary.com/v0/define?term="+`${request.params.term}`);
+
+        // Get the HTML of the page
+        let html = await page.evaluate(() => document.body.innerHTML);
+        var parsed = html.split("\"");
+
+        // Initialize the definition
+        var definition;
+
+        // There is no definition
+        if(parsed[7] == null || !html.includes(`${request.params.term}`))
+            definition = null;
+
+        // The definition had quotes in it (the easy case)
+        else if(parsed[8] != ",") {
+            // Get the unsanitized definition
+            definition = parsed[8].toLowerCase();
+
+            // Remove square brackets and backslashes
+            definition = definition.replace(/[\[\]\\']+/g,'')
+        }
+
+        // The definition needs to heavily sanitized
+        else {
+            // Get the unsanitized definition
+            definition = parsed[7].toLowerCase();
+
+            // Remove square brackets
+            definition = definition.replace(/[\[\]']+/g,'')
+
+            // Remove the word itself being used in the definition
+            definition = definition.replace(`${request.params.term}`, "");
+
+            // Remove any instance of introduction phrases (should be ending in a space)
+            var introPhases = [
+                "when something is ",
+                "word used instead of ",
+                "a word used instead of ",
+                "acronym for ",
+                "is a hybrid between ",
+                "a hybrid between ",
+                "is another way of saying ",
+                "another way of saying ",
+                "1) ",
+                "1)",
+                "1. ",
+                "1.",
+                "&lt;",
+                "(as a verb) ",
+                "(as a noun) ",
+                "(as an adjective) ",
+                "similar to ",
+            ]
+            for(var i = 0; i < introPhases.length; i++)
+                definition = definition.replace(introPhases[i], "");
+            
+            // Get the sentence fragment before any puncuation
+            var puncuations = [
+                ".",
+                ",",
+                ";",
+                "--",
+                "\\r",
+            ]
+            for(var i = 0; i < puncuations.length; i++)
+                [definition] = definition.split(puncuations[i]);
+
+        // Remove an introductory words (should be ending in a space)
+        var introWords = [
+            "to ",
+            "a ",
+            "any ",
+        ]
+        for(var i = 0; i < introWords.length; i++) 
+            if(definition.startsWith(introWords[i]))
+                definition = definition.replace(introWords[i], "");
+
+        // Get the lefthand side of an or statement
+        [definition] = definition.split(" or ");
+        [definition] = definition.split(" but ");
+
+        // Replace + with and
+        definition = definition.replace("+", "and");
+        }
+
+        await browser.close();
+
+        // If the definition is not null, remove most punctuation
+        (definition == null)? definition = definition : definition = definition.replace(/[^\w\s']|_/g, "");
+
+        // Send the definition
+        response.send(definition);
     })
 })
--- public/js/translate.js
+++ public/js/translate.js
@@ -1,37 +1,85 @@
+/**
+ * Translate the contents of the slang text area and put it in the formal text area
+ */
 async function doTranslation() {
     // Get the setence in the slang box.
     var slangText = document.getElementById("slang").value;
 
-    // Clear the text in the formal box
-    document.getElementById("formal").value = " ";
+    // Clear the text in the formal box and translation list
+    reset();
 
     // If the slang box is empty, return
-    if(!slangText) {
-        document.getElementById("formal").value = null;
+    if(!slangText)
         return;
-    }
 
     // Get the individual words of the setence in all lowercase.
     var tokens = tokenize(slangText);
 
-    // Put the first token into the formal box
-    var response = await fetch('query/'+tokens[0]);
-    var data = await response.json();
-    if(!isEmpty(data))
-        document.getElementById("formal").value = `${data[0].translation}`;
-    else
-        document.getElementById("formal").value = tokens[0];
+    // Form the translated sentence
+    var currQuery, workingSentence = "", currTerm;;
+    for(var i = 0; i < tokens.length; i++) {
+        // Create a clean version of the current term
+        currTerm = tokens[i].toLowerCase().replace(/[^\w\s']|_/g, "");
 
-    // Append the remaining tokens into the formal box
-    for(var i = 1; i < tokens.length; i++) {
-        response = await fetch('query/'+tokens[i]);
-        data = await response.json();
-        if(!isEmpty(data))
-            document.getElementById("formal").value = document.getElementById("formal").value + " " + `${data[0].translation}`;
-        else
-            document.getElementById("formal").value = document.getElementById("formal").value + " " + tokens[i];
+        // If the current term was all special characters, continue
+        if(!currTerm)
+            continue;
+
+        // Fetch the query of the current term
+        currQuery = await fetchQuery(currTerm);
+
+        // If the term is not in the dictionary, try to fetch the translation
+        if(isEmpty(currQuery)) {
+            currQuery = await scrapeDefinition(currTerm);
+            // If the scrap was unsuccessful, add the to the unknown list and append the token
+            if(!currQuery) {
+                document.getElementById("unknownlist").innerHTML += currTerm + "<br/>";
+                workingSentence += " " + tokens[i];
+            }
+            // Else, append the scrapped definition and add it to the translation list
+            else {
+                document.getElementById("translationlist").innerHTML += currTerm + " = " + currQuery + "*<br/>";
+                workingSentence+= " " + currQuery;
+                
+                // If there is punctuation, add it
+                if(currTerm != tokens[i])
+                    workingSentence += tokens[i].charAt(tokens[i].length - 1);
+            }
+        }
+
+        // If the term is in the dictionary, append the translation
+        else {
+            // If the term is slang, add it to the translation list
+            if(`${currQuery[0].isSlang}` == 1) {
+                document.getElementById("translationlist").innerHTML += currQuery[0].term + " = " + currQuery[0].translation + "<br/>";
+                workingSentence += " " + currQuery[0].translation;
+
+                // If there is punctuation, add it
+                if(currTerm != tokens[i])
+                    workingSentence += tokens[i].charAt(tokens[i].length - 1);
+            }
+            else
+                workingSentence += " " + tokens[i];
+
+        }
     }
 
+    // Remove the first character of the final sentece, since it is a space
+    workingSentence = workingSentence.substring(1);
+    document.getElementById("formal").value = workingSentence;
+
+    // Add the disclaimer if UrbanDictionary translations were used
+    if(document.getElementById("translationlist").innerHTML.includes("*"))
+        document.getElementById("translationlist").innerHTML += "<br/>Definitions with * were fetched <br/> from UrbanDictionary <br/>";
+}
+
+/**
+ * Clear the text in the formal box and translation list
+ */
+function reset() {
+    document.getElementById("formal").value = "";
+    document.getElementById("translationlist").textContent = null;
+    document.getElementById("unknownlist").textContent = null;
 }
 
 /** 
@@ -44,11 +92,8 @@ function tokenize(str) {
 
     // For each word in the setence...
     for(var i = 0; i < tokens.length; i++) {
-        // Change all characters to lower case
-        tokens[i] = tokens[i].toLowerCase();
-
-        // Strip any punctuation
-        tokens[i] = tokens[i].replace(/[^\w\s']|_/g, "");
+        // Sanitize the input to prevent sql injection
+        tokens[i] = tokens[i].replace("'", "\'")
     }
 
     return tokens;
@@ -61,10 +106,38 @@ function tokenize(str) {
  * @returns Whether or not the JSON object is empty
  */
 function isEmpty(obj) {
-    for(var prop in obj) {
+    for(var prop in obj)
         if(obj.hasOwnProperty(prop))
             return false;
-    }
 
     return true;
+}
+
+/**
+ * Query the database for some term.
+ * 
+ * @param {String} token The term to query for.
+ * @returns The results of the query as a `JSON`.
+ */
+async function fetchQuery(token) {
+    var response = await fetch('query/'+token);
+    var data = await response.json();
+    
+    return data;
+}
+
+/**
+ * Scrape a defintion of a term from UrbanDictionary
+ * 
+ * @param {String} term The term to autodefine.
+ * 
+ * @returns The scrapped definition of term, or null if none was found
+ */
+async function scrapeDefinition(term) {
+    // Probably some fetch
+    var response = await fetch('scrape/'+term);
+    var data = await response.text();
+
+    // Return the definition
+    return data;
 }
